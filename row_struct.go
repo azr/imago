4f25bbf4570a19a5fc0d6c15bf4f0d5dc3c2cd45
Adrien Delorme <adrien.delorme@icloud.com>
2015-04-11 12:58:58 +0200

    refactor(GetRow): use PixelSize instead of hard coded values

--- row_struct.go
+++ row_struct.go
@@ -26,63 +26,63 @@ func GetRow(i image.Image, y int) *Row {
 	case *image.Alpha:
 		return &Row{
 			Pix:       img.Pix[img.PixOffset(0, y) : img.PixOffset(0, y)+img.Stride],
-			PixelSize: 1,
+			PixelSize: PixelSize(img),
 			Stride:    img.Stride,
 			From:      i,
 		}
 	case *image.Alpha16:
 		return &Row{
 			Pix:       img.Pix[img.PixOffset(0, y) : img.PixOffset(0, y)+img.Stride],
-			PixelSize: 2,
+			PixelSize: PixelSize(img),
 			Stride:    img.Stride,
 			From:      i,
 		}
 	case *image.Gray:
 		return &Row{
 			Pix:       img.Pix[img.PixOffset(0, y) : img.PixOffset(0, y)+img.Stride],
-			PixelSize: 1,
+			PixelSize: PixelSize(img),
 			Stride:    img.Stride,
 			From:      i,
 		}
 	case *image.Gray16:
 		return &Row{
 			Pix:       img.Pix[img.PixOffset(0, y) : img.PixOffset(0, y)+img.Stride],
-			PixelSize: 1,
+			PixelSize: PixelSize(img),
 			Stride:    img.Stride,
 			From:      i,
 		}
 	case *image.NRGBA:
 		return &Row{
 			Pix:       img.Pix[img.PixOffset(0, y) : img.PixOffset(0, y)+img.Stride],
-			PixelSize: 4,
+			PixelSize: PixelSize(img),
 			Stride:    img.Stride,
 			From:      i,
 		}
 	case *image.NRGBA64:
 		return &Row{
 			Pix:       img.Pix[img.PixOffset(0, y) : img.PixOffset(0, y)+img.Stride],
-			PixelSize: 8,
+			PixelSize: PixelSize(img),
 			Stride:    img.Stride,
 			From:      i,
 		}
 	case *image.Paletted:
 		return &Row{
 			Pix:       img.Pix[img.PixOffset(0, y) : img.PixOffset(0, y)+img.Stride],
-			PixelSize: 1,
+			PixelSize: PixelSize(img),
 			Stride:    img.Stride,
 			From:      i,
 		}
 	case *image.RGBA:
 		return &Row{
 			Pix:       img.Pix[img.PixOffset(0, y) : img.PixOffset(0, y)+img.Stride],
-			PixelSize: 4,
+			PixelSize: PixelSize(img),
 			Stride:    img.Stride,
 			From:      i,
 		}
 	case *image.RGBA64:
 		return &Row{
 			Pix:       img.Pix[img.PixOffset(0, y) : img.PixOffset(0, y)+img.Stride],
-			PixelSize: 8,
+			PixelSize: PixelSize(img),
 			Stride:    img.Stride,
 			From:      i,
 		}
