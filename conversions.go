0f3eb67f66f8b00cde0624769667907b3c40c884
Adrien Delorme <adrien.delorme@icloud.com>
2015-04-12 14:38:12 +0200

    feat(ToFloat64): add func to convert image.Image to []float64

---
+++ conversions.go
@@ -0,0 +1,24 @@
+package imago
+
+import (
+	"image"
+)
+
+func ToFloat64(i image.Image) []float64 {
+	return PixelsToFloat64(GetPix(i), PixelSize(i))
+}
+
+func PixelsToFloat64(Pix []uint8, PixelSize int) []float64 {
+	plen := len(Pix)
+	res := make([]float64, 0, plen/PixelSize)
+	for i := 0; i < plen; i += PixelSize {
+		var v uint64
+		for n, j := 0, PixelSize-1; j >= 0; j-- {
+			v += uint64(Pix[i+j]) << uint64(n*8)
+			n++
+		}
+		res = append(res, float64(v))
+	}
+	return res
+}
+
