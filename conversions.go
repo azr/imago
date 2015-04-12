004342c1930756e5eef2ea3fde93d48df110bb5a
Adrien Delorme <adrien.delorme@icloud.com>
2015-04-12 14:38:38 +0200

    feat(ToComplex): add func to convert image.Image to []complex128

--- conversions.go
+++ conversions.go
@@ -22,3 +22,20 @@ func PixelsToFloat64(Pix []uint8, PixelSize int) []float64 {
 	return res
 }
 
+func ToComplex(i image.Image) []complex128 {
+	return PixelsToComplex(GetPix(i), PixelSize(i))
+}
+
+func PixelsToComplex(Pix []uint8, PixelSize int) []complex128 {
+	plen := len(Pix)
+	res := make([]complex128, 0, plen/PixelSize)
+	for i := 0; i < plen; i += PixelSize {
+		var v uint64
+		for n, j := 0, PixelSize-1; j >= 0; j-- {
+			v += uint64(Pix[i+j]) << uint64(n*8)
+			n++
+		}
+		res = append(res, complex(float64(v), 0))
+	}
+	return res
+}
